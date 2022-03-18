1bb837fce69cc7ca1da56acb2a65293fc94a1f63
fonCki <alfonsoridao@gmail.com>
2022-03-18 17:36:05 +0100

    sign up

--- Entities/Model/User.cs
+++ Entities/Model/User.cs
@@ -10,7 +10,12 @@ public class User {
     }
 
     public string UserName { get; set; }
+    
+    public string LastName { get; set; }
+    
+    public string Email { get; set; }
     public string Password { get; set; }
+    
     
     
 }
--- JsonDataAccess/JsonUserDAO.cs
+++ JsonDataAccess/JsonUserDAO.cs
@@ -21,31 +21,37 @@ public class JsonUserDAO : IUserDAO{
     public async Task<ICollection<User>> GetUsersAsync() {
         return jsonContext.Forum.Users;
     }
+    
 
-    public async Task<User> GetByUserName(string userName) {
-        return jsonContext.Forum.Users.First(user => user.UserName == userName);
-        
+    public async Task<User> GetByUserByEmail(string email) {
+        return jsonContext.Forum.Users.First(user => user.Email == email);
+
     }
 
     public async Task<User> AddUserAsync(User user) {
-        // if (GetByUserName(user.UserName).Equals(null)) {
+        try {
+            await GetByUserByEmail(user.Email);
+            Console.WriteLine("hola");
+            throw new Exception("user exist");
+        }
+        catch (Exception e) {
             jsonContext.Forum.Users.Add(user);
             await jsonContext.SaveChangesAsync();
             Console.WriteLine("Writed");
             return user;
-      //  }
-      //  return null;
+        }
     }
 
-    public async Task DeleteUserAsync(string userName) {
-        jsonContext.Forum.Users.Remove(GetByUserName(userName).Result);
+    public async Task DeleteUserAsync(string email) {
+        jsonContext.Forum.Users.Remove(GetByUserByEmail(email).Result);
         jsonContext.SaveChangesAsync();
     }
 
     public async Task UpdateUserAsync(User user) {
-        User userToUpdate = GetByUserName(user.UserName).Result;
+        User userToUpdate = GetByUserByEmail(user.UserName).Result;
         userToUpdate.Password = user.Password;
         jsonContext.SaveChangesAsync();
     }
 
+    
 }
